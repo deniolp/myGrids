06b20b1b4d9d6ba8fb2894869a5bd0f59df6564d
Denis Popov <deniolp@gmail.com>
2019-01-18 15:11:08 +0300

    Making gulpfile.js in this project

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+let gulp = require(`gulp`);
+let server = require(`browser-sync`).create();
+
+gulp.task(`serve`, function () {
+  server.init({
+    server: ``,
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch(`*.css`).on(`change`, server.reload);
+  gulp.watch(`*.html`).on(`change`, server.reload);
+});
